132ec8eba8c5cfac043d4a82b442e69eedd108de
Felix Shaw <felix.shaw@tgac.ac.uk>
2016-03-10 16:47:33 +0000

    skeleton backend file added

---
+++ backend.py
@@ -0,0 +1,18 @@
+from pybrain.tools.shortcuts import buildNetwork
+
+class Backend:
+
+    net = buildNetwork(2, 3, 1)
+
+    def get_table_data():
+        '''
+            get pre-boosted data for front-end display
+        '''
+
+    def update_net(ids=[]):
+        '''
+            ids is an array of document ids the features of which
+            will be used to update the classifier
+        '''
+
+    
