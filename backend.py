8bbef709c97be6b80e5b921a59c792e3531592ca
Toni Etuk <tonietuk@yahoo.com>
2016-03-11 15:13:39 +0000

    paper data added

--- backend.py
+++ backend.py
@@ -1,33 +1,35 @@
 from pybrain.tools.shortcuts import buildNetwork
 from pycproject.readctree import CProject, CTree
 import os
+import os.path
+import json
 
 class Backend:
 
-    # net = buildNetwork(2, 3, 1)
+    typenet = buildNetwork(2, 3, 1)
 
-    def get_table_data():
+    def get_table_data(self):
         '''
             get pre-boosted data for front-end display
         '''
-        return ["Battling Zika in Brazil",
-                       "A crucial time for public health preparedness: Zika virus and the 2016 Olympics, Umrah, and Hajj",
-                       "Offline: Brazil-the unexpected opportunity that Zika presents",
-                       "Zika Virus on the Move.",
-                       "Zika, and rapid diagnostic tests for malaria",
-                       "Research bodies vow to share data on Zika",
-                       "Scientists probe Zika link to birth defects",
-                       "Proving Zika link to birth defects poses huge challenge.",
-                       "Severe eye damage in infants with microcephaly is presumed to be due to Zika virus",
-                       "Healthcare staff encouraged to warn patients of the risks of the Zika virus"]
 
-    def update_net(ids=[]):
+        paper_list = [] 
+        for td in self.generate_test_data("testdata"):
+            paper_list.append(td)
+
+        return paper_list  
+
+    def update_net(self, ids=[]):
         '''
             ids is an array of document ids the features of which
             will be used to update the classifier
         '''
 
-    def generate_test_data(inputfolder):
-        testdata = CProject(os.getcwd(),"testdata")
+    def generate_test_data(self, inputfolder):
+        curdir = os.getcwd()
+        testdata = CProject(curdir, "testdata")
         for ctree in testdata.get_ctrees():
             yield ctree.get_classifier_features()
+            
+b = Backend()
+print(b.get_table_data())
