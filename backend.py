bf85c1bde8fa60ab321fdbb2f0541a8ae07f5dbc
Felix Shaw <felix.shaw@tgac.ac.uk>
2016-03-11 15:18:55 +0000

    nets saving

--- backend.py
+++ backend.py
@@ -1,12 +1,25 @@
 from pybrain.tools.shortcuts import buildNetwork
+from pybrain.datasets import SupervisedDataSet
+from pybrain.supervised.trainers import BackpropTrainer
 from pycproject.readctree import CProject, CTree
 import os
 
 class Backend:
 
-    net = buildNetwork(2, 3, 1)
+    def __init__(self):
+        # first try and load saved network
+        try:
+            fileObject = open('neural_net.json','r')
+        except FileNotFoundError:
+            net = buildNetwork(2, 3, 1)
+            fileObject = open('neural_net.json','w')
+            pickle.dump(net, fileObject)
 
-    def get_table_data():
+    def get_net(self):
+        fileObject = open('neural_net.json', 'w')
+        return pickle.load(fileObject)
+
+    def get_table_data(self):
         '''
             get pre-boosted data for front-end display
         '''
@@ -21,11 +34,14 @@ class Backend:
                        "Severe eye damage in infants with microcephaly is presumed to be due to Zika virus",
                        "Healthcare staff encouraged to warn patients of the risks of the Zika virus"]
 
-    def update_net(ids=[]):
+    def update_net(self, ids=[]):
         '''
             ids is an array of document ids the features of which
             will be used to update the classifier
         '''
+        net = get_net()
+        
+
 
     def generate_test_data(inputfolder):
         testdata = CProject(os.getcwd(),"testdata")
